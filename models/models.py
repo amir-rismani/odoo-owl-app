b99d73770e97a4c228290721fd38c490f0d4e086
Amir Rismaani <amir.rismaani@gmail.com>
2023-11-23 08:25:40 +0330

    create sequence number for todos

--- models/models.py
+++ models/models.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 
 
-class odoo_owl_app(models.Model):
+class OwlApp(models.Model):
     _name = 'odoo_owl_app.todo'
     _description = 'Odoo OWL App Todo'
-
+    name = fields.Char(string='Todo Reference', copy=False, readonly=True, index=True, default=lambda self: _('New'))
     text = fields.Char()
     is_completed = fields.Boolean()
+    
+    
+    @api.model
+    def create(self, vals):
+        vals['name'] = self.env['ir.sequence'].next_by_code('odoo_owl_app.todo') # odoo_owl_app.todo is code value 
+        return super(OwlApp, self).create(vals)
