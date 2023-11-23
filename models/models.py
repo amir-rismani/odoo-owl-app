5e68f0d5eb24c218152d420e6fc85b1ec4bd3292
Amir Rismaani <amir.rismaani@gmail.com>
2023-11-23 07:03:40 +0330

    add model

--- models/models.py
+++ models/models.py
@@ -1,18 +1,11 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class odoo_owl_app(models.Model):
-#     _name = 'odoo_owl_app.odoo_owl_app'
-#     _description = 'odoo_owl_app.odoo_owl_app'
+class odoo_owl_app(models.Model):
+    _name = 'odoo_owl_app.todo'
+    _description = 'Odoo OWL App Todo'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    text = fields.Char()
+    is_completed = fields.Boolean()
